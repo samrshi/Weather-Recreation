8119bdeb3ab3325d36e30fd6c0df99492129e953
samrshi <samrshi@outlook.com>
2021-04-12 23:10:50 -0400

    code styling

--- WeatherWidget/WidgetLocationManager.swift
+++ WeatherWidget/WidgetLocationManager.swift
@@ -39,23 +39,18 @@ class WidgetLocationManager: NSObject, CLLocationManagerDelegate {
   }
   
   func lookUpLocationName(completionHandler: @escaping (CLPlacemark?) -> Void ) {
-    // Use the last reported location.
     if let lastLocation = self.locationManager?.location {
       let geocoder = CLGeocoder()
       
-      // Look up the location and pass it to the completion handler
-      geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+      geocoder.reverseGeocodeLocation(lastLocation) { (placemarks, error) in
         if error == nil {
           let firstLocation = placemarks?[0]
           completionHandler(firstLocation)
-        }
-        else {
-          // An error occurred during geocoding.
+        } else {
           completionHandler(nil)
         }
-      })
+      }
     } else {
-      // No location was available.
       completionHandler(nil)
     }
   }
